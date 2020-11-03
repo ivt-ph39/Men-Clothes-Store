a34161fa0b505ef63e53f5c35a5e117b409e8ec8
neit27 <tiennguyengl27@gmail.com>
2020-11-03 21:50:07 +0700

    middleware

--- app/Order.php
+++ app/Order.php
@@ -10,6 +10,14 @@ class Order extends Model
     [
     	'name', 'status', 'total', 'email', 'address', 'phone', 'message'
     ];
+    const ORDER_STATUS = [
+        0 => 'Đang đóng gói',
+        1 => 'Đã đóng gói',
+        2 => 'Đang giao',
+        3 => 'Đã giao',
+        4 => 'Đã hủy',
+        5 => 'Khách không nhận'
+    ];
     public function orderDetail()
     {
     	return $this->hasMany('App\OderDetail');
